c53d1a537427e18801ba7fe739fb928a1b3cbfb8
chenxu <chenxu2048@gmail.com>
2017-12-11 16:07:15 +0800

    未添加点赞和评论

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,6 +21,9 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      wx.switchTab({
+        url: '/pages/Main/Main',
+      })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -43,6 +46,9 @@ Page({
             hasUserInfo: true
           })
         }
+      })
+      wx.switchTab({
+        url: '/pages/Main/Main',
       })
     }
   },
