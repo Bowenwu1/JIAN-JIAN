61227e894febed61e9dff8bba4d8edf93d0eb045
BowenWu <bowen.wu@163.com>
2017-12-14 00:26:07 +0800

    finish raft page

---
+++ pages/raft/raft.js
@@ -0,0 +1,88 @@
+// pages/raft/raft.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    raft_book_list:[
+      {
+        book_owner_avator: '../../images/sample-avator.png',
+        book_owner_name:'藤椒兔',
+        book_avator: '../../images/example3.jpg',
+        book_name:'《檀香刑》莫言',
+        book_isbn:'ISBN:9787505735446'
+      },
+      {
+        book_owner_avator: '../../images/sample-avator.png',
+        book_owner_name: '藤椒兔',
+        book_avator: '../../images/example3.jpg',
+        book_name: '《檀香刑》莫言',
+        book_isbn: 'ISBN:9787505735446'
+      },
+      {
+        book_owner_avator: '../../images/sample-avator.png',
+        book_owner_name: '藤椒兔',
+        book_avator: '../../images/example3.jpg',
+        book_name: '《檀香刑》莫言',
+        book_isbn: 'ISBN:9787505735446'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
