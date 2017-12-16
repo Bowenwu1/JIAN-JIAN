9d711c6042f3bcacfaffcf6d27ee867013e9a093
Jonathan Yang <cjyk84719@gmail.com>
2017-12-16 16:47:09 +0800

    dynamical avatar&username

--- pages/Mine/Mine.js
+++ pages/Mine/Mine.js
@@ -7,38 +7,39 @@ Page({
    */
   data: {
     bookshelf_image:[
-      '../../images/example1.jpg',
-      '../../images/example2.jpg',
-      '../../images/example3.jpg',
-      '../../images/example1.jpg',
-      '../../images/example2.jpg',
-      '../../images/example3.jpg'
+      '/images/example1.jpg',
+      '/images/example2.jpg',
+      '/images/example3.jpg',
+      '/images/example1.jpg',
+      '/images/example2.jpg',
+      '/images/example3.jpg'
     ],
     join_community:[
       {
-        avator: '../../images/example1.jpg',
+        avator: '/images/example1.jpg',
         title: '《有一种候鸟》陈少聪',
         owner:'群主：BOB',
         number_partner:'群成员：4人'
       },
       {
-        avator: '../../images/example2.jpg',
+        avator: '/images/example2.jpg',
         title: '《毛概》马爱云',
         owner: '群主：藤椒兔',
         number_partner: '群成员：4人'
       },
       {
-        avator: '../../images/example3.jpg',
+        avator: '/images/example3.jpg',
         title: '《东方快车》阿加莎·克里斯蒂',
         owner: '群主：藤椒鸡',
         number_partner: '群成员：4人'
       }
     ],
-    personal_avator: '../../images/sample-avator.png',
+    personal_avator: '/images/sample-avator.png',
     username:'藤椒兔',
     read_book:3,
     read_hour:20,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo: {}
   },
 
   /**
@@ -46,23 +47,41 @@ Page({
    */
   onLoad: function (options) {
     console.log('onLoad')
-    if (this.data.canIUse) {
-      app.userInfoReadyCallback = res => {
+    // if (this.data.canIUse) {
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       username:res.userInfo.nickName,
+    //       personal_avator: res.userInfo.avatarUrl
+    //     })
+    //     conosle.log(res);
+    //   }
+    // } else {
+    //   wx.getUserInfo({
+    //     success:res=> {
+    //       this.setData({
+    //         username: res.userInfo.nickName,
+    //         personal_avator: res.userInfo.avatarUrl
+    //       })
+    //       conosle.log(res);
+    //     },
+    //     fail:function() {
+    //       console.log('fail')
+    //     }
+    //   })
+    // }
+
+    wx.getUserInfo({
+      success:res=> {
         this.setData({
-          username:res.userInfo.nickName,
+          username: res.userInfo.nickName,
           personal_avator: res.userInfo.avatarUrl
         })
+        console.log(res);
+      },
+      fail:function() {
+        console.log('fail')
       }
-    } else {
-      wx.getUserInfo({
-        success:res=> {
-          this.setData({
-            username: res.userInfo.nickName,
-            personal_avator: res.userInfo.avatarUrl
-          })
-        }
-      })
-    }
+    })
   },
 
   /**
