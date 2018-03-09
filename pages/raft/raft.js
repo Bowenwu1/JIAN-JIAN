5e0db867cc99fd1cb367e2df76e3276a0e5ba2b7
Llunnn <gdsgwzj@163.com>
2018-03-09 19:17:16 +0800

    something

--- pages/raft/raft.js
+++ pages/raft/raft.js
@@ -7,14 +7,14 @@ Page({
   data: {
     raft_book_list:[
       {
-        book_owner_avator: '../../images/sample-avator.png',
+        book_owner_avator: '../../images/avatar1.jpg',
         book_owner_name:'藤椒兔',
         book_avator: '../../images/txx.jpg',
         book_name:'《檀香刑》莫言',
         book_isbn:'ISBN:9787505735446'
       },
       {
-        book_owner_avator: '../../images/sample-avator.png',
+        book_owner_avator: '../../images/avatar1.jpg',
         book_owner_name: '藤椒兔',
         book_avator: '../../images/mss.jpg',
         book_name: '《写给大家的西方美术史》蒋勋',
